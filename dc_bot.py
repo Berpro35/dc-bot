58e6dd978b4544fe7eef27daa12f5452c28a0f16
Berpro35 <154363594+Berpro35@users.noreply.github.com>
2024-01-23 21:49:24 +0300

    Create dc_bot.py

---
+++ dc_bot.py
@@ -0,0 +1,31 @@
+import discord
+
+intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    
+    if message.author == client.user:
+        return
+    if message.content.startswith('telefon kartı'):
+        await message.channel.send("1000 yılda kaybolur")
+    elif message.content.startswith('pil'):
+        await message.channel.send("300 yılda kaybolur")
+    elif message.content.startswith('alüminyum'):
+        await message.channel.send("100 yılda kaybolur")
+    elif message.content.startswith('plastik'):
+        await message.channel.send("1000 yılda kaybolur")
+    elif message.content.startswith('karton kutu'):
+        await message.channel.send("2 ayda kaybolur")
+    elif message.content.startswith('atıklarla ilgili bilgi'):
+        await message.channel.send("telefon kartı,pil,alüminyum,plastik,karton kutu lütfen aralarından birini seçin onun dışındakiler çalışmayacaktır :)")
+    else:
+        await message.channel.send(message.content)
+
+client.run("MTE5MTgwMzY4NTM5OTk1NzU3NA.GDQKgW.3-IUKGWJD-W-Rx1ItOvRKzq5Z4RB2knFkLkgig")
